fa4c73d413788c632f88de4521e8773c36ef6407
KarnatiAlekhya <109645697+KarnatiAlekhya@users.noreply.github.com>
2022-09-06 12:04:12 +0530

    Add files via upload

---
+++ phase3/practiceproject/SpringSecurity/src/main/java/com/junit/DemoController.java
@@ -0,0 +1,14 @@
+package com.junit;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+@ResponseBody
+	@RequestMapping("/")
+	public String demo() {
+		return "hi simplilearn users";
+	}
+	
+}
